39fc8f0abaf302ec1ab90525b2c770148bf5f099
DStow <Dan_Stow@live.co.uk>
2026-03-20 18:38:44 +0000

    Extract the mouse reticle to top level

--- scripts/tank.gd
+++ scripts/tank.gd
@@ -2,14 +2,14 @@ extends CharacterBody2D
 
 @export var turn_speed: float = 0.9
 @export var turret_rotation_speed: float = 2.6
-var forward_velocity = 0
-var acceleration = 100
-var max_velocity = 150
+@export var targetting_reticle: TargettingReticle
+
+var forward_velocity: float = 0
+var acceleration: int = 100
+var max_velocity: int = 150
 
 var mouse_global_pos: Vector2 = Vector2.ZERO
 
-# var turret_desired_position: Vector2 = Vector2.ZERO
-@onready var mouse_pos: MouseReticle = $MouseReticle
 @onready var projectile_spawn_position: Marker2D = $Turret/ProjectileSpawnPosition
 @onready var turret: Sprite2D = $Turret
 @onready var laser_spawn_position: Marker2D = $Turret/LaserSpawnPosition
@@ -18,14 +18,11 @@ var mouse_global_pos: Vector2 = Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	
+	targetting_reticle.set_active_reload_component(reload_component)
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	mouse_pos.position = to_local(mouse_global_pos)
-	
 	if Input.is_action_pressed("turn_left"):
 		rotate(-1 * turn_speed * delta)
 	elif Input.is_action_pressed("turn_right"):
@@ -45,17 +42,17 @@ func _process(delta: float) -> void:
 
 	forward_velocity = clampf(forward_velocity, -max_velocity, max_velocity)
 
-	var motion_vector = Vector2(forward_velocity, 0).rotated(rotation)
+	var motion_vector: Vector2 = Vector2(forward_velocity, 0).rotated(rotation)
 	
 	velocity = motion_vector
-	var collided = move_and_slide()
+	var collided: bool = move_and_slide()
 	if collided and forward_velocity > 20:
 		forward_velocity = 20
 	
 	if(!is_turret_facing_desired()):
-		var required_rotation = get_turret_rotation_to_target(mouse_pos.position)
+		var required_rotation: float = get_turret_rotation_to_target(targetting_reticle.global_position)
 
-		var time_to_complete = snappedf(abs(required_rotation) / turret_rotation_speed, 0.1)
+		var time_to_complete: float = snappedf(abs(required_rotation) / turret_rotation_speed, 0.1)
 		time_to_target.text = str(time_to_complete) + "s"
 		required_rotation = clampf(required_rotation, turret_rotation_speed * -1 * delta, turret_rotation_speed * delta)
 
@@ -63,34 +60,25 @@ func _process(delta: float) -> void:
 
 	# Now check again after the rotation has been applied
 	if(!is_turret_facing_desired()):
-		mouse_pos.set_looking_at_target(false)
+		targetting_reticle.set_looking_at_target(false)
 		time_to_target.visible = false
-		time_to_target.global_position = mouse_pos.global_position
+		time_to_target.global_position = targetting_reticle.global_position
 	else:
-		mouse_pos.set_looking_at_target(true)
+		targetting_reticle.set_looking_at_target(true)
 		time_to_target.visible = false
 		
 func get_turret_rotation_to_target(target: Vector2) -> float:
-	var direction = (target - turret.position).normalized()
-	var current_rotation = turret.rotation
-	var change = direction.angle() - current_rotation
+	var direction: Vector2 = (target - turret.global_position).normalized()
+	var current_rotation: float = turret.global_rotation
+	var change: float = direction.angle() - current_rotation
 	return wrapf(change, -PI, PI)
 	
 func is_turret_facing_desired() -> bool:
-	var rotation_required = get_turret_rotation_to_target(mouse_pos.position)
+	var rotation_required: float = get_turret_rotation_to_target(targetting_reticle.global_position)
 	return abs(rotation_required) < 0.01
 	
 func _input(event):
-	if event is InputEventMouseMotion:
-		mouse_global_pos = event.global_position
-		# Apply the camera offset here
-		
-	
 	if event is InputEventMouseButton and event.pressed:
 		if event.button_index == MOUSE_BUTTON_LEFT and is_turret_facing_desired() and reload_component.can_fire() :
 			reload_component.fire()
 			ProjectileManager.spawn_projectile(projectile_spawn_position.global_position, turret.global_rotation)
-
-#func _draw() -> void:
-	#var dest = laser_spawn_position.position + Vector2(200, 0).rotated(turret.rotation)
-	#draw_line(laser_spawn_position.position, dest, Color.DARK_RED, -1, true)
--- scripts/targetting_reticle.gd
+++ scripts/targetting_reticle.gd
@@ -1,19 +1,22 @@
 extends Node2D
 
-class_name MouseReticle
+class_name TargettingReticle
 
-@export var reload_component: ReloadComponent
 @onready var cursor_sprite: Sprite2D = $CursorSprite
 
-var _looking_at_target
+var _looking_at_target: bool = false
+var _reload_component: ReloadComponent = null
+var _mouse_global_pos: Vector2 = Vector2.ZERO
 
 func _ready() -> void:
 	pass # Replace with function body.
 
 
 func _process(delta: float) -> void:
-	rotation = -get_parent().global_rotation
-	queue_redraw()
+	if global_position != _mouse_global_pos:
+		queue_redraw()
+	
+	global_position = _mouse_global_pos
 
 func set_looking_at_target(value: bool) -> void:
 	if _looking_at_target == value:
@@ -21,8 +24,7 @@ func set_looking_at_target(value: bool) -> void:
 	_looking_at_target = value
 	cursor_sprite.modulate = Color.GREEN if value else Color.RED
 	queue_redraw()
-	
-	
+
 func _draw() -> void:
 	var radius: float = 32.0
 	var thickness: float = 6.0
@@ -30,11 +32,18 @@ func _draw() -> void:
 
 	draw_arc(Vector2.ZERO, radius, 0, TAU, segments, Color(0, 0, 0, 0.4), thickness)
 	
-	if !reload_component.is_reloading():
+	if !_reload_component.is_reloading():
 		var color: Color = Color.WEB_GREEN
 		if !_looking_at_target: color = Color.RED
 		draw_arc(Vector2.ZERO, radius, 0, TAU, segments, color, thickness)
 	else:
 		var start_angle: float = -PI / 2
-		var end_angle: float = start_angle + (TAU * reload_component.reload_progress())
+		var end_angle: float = start_angle + (TAU * _reload_component.reload_progress())
 		draw_arc(Vector2.ZERO, radius, start_angle, end_angle, segments, Color.RED, thickness)
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		_mouse_global_pos = event.global_position
+
+func set_active_reload_component(reload_component: ReloadComponent) -> void:
+	_reload_component = reload_component
